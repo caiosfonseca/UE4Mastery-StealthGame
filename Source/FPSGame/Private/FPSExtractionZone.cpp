47fe42d22976ac7a6234b2e66126e6687103c26d
Caio Souza Fonseca <caiosfonseca@hotmail.com>
2020-05-11 13:15:59 +0200

    Added course provided materials for the extraction zone. Added the actual extraction zone. Implemented new function on the gamemode to finish the mission if the player has the objective and stepped int the extraction zone.

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(Size));
+	RootComponent = OverlapComp;
+
+	OverlapComp->SetHiddenInGame(false);
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->DecalSize = FVector(Size);
+	DecalComp->SetupAttachment(RootComponent);
+}
+
+void AFPSExtractionZone::HandleOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
+
+	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
+	if(IsValid(MyPawn) && MyPawn->bIsCarryingObjective)
+	{
+		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if(IsValid(GM))
+		{
+			GM->CompleteMission(MyPawn);
+		}
+	}
+}
